88be1183768a1dee84d2eb843fe9c4298aed7633
Dmitry <hahlek@gmail.com>
2020-12-22 17:12:56 +0700

    journal.html

--- app/js/app.js
+++ app/js/app.js
@@ -70,5 +70,87 @@ document.addEventListener('DOMContentLoaded', () => {
 	let toggler = document.querySelector('.toggler')
 	toggler.addEventListener('click', menuToggle)
 
+	function filter(item, text) {
+		let block = document.querySelector(`#${item}`)
+		let news = document.querySelectorAll(`.filterBlock .horizontalBar > .${item}`)
+		let catBlock = document.querySelector('.journal__block1LeftNewsCategories')
 
+		if(block.classList.contains('filtered')) {
+			block.classList.remove('filtered')
+			let cat = document.querySelector(`#blockCat > span.${item}`)
+			cat.remove()
+			for(let bl of news) {
+				bl.classList.remove('filtered')
+			}
+			let anyone = document.querySelectorAll('.journal__block1LeftSections .sections__blocksBlock.filtered')
+			if(anyone.length === 0) {
+				for (let i of mainBlock) {
+					i.classList.remove('filtered')
+				}
+				catBlock.classList.remove('disp')
+			}
+			return
+		}
+
+		catBlock.classList.add('disp')
+		block.classList.add('filtered')
+		categories.insertAdjacentHTML('beforeend', `<span class="${item}">${text}</span>`)
+		for (let i of mainBlock) {
+			i.classList.add('filtered')
+		}
+		for(let bl of news) {
+			bl.classList.add('filtered')
+		}
+
+	}
+	let mainBlock = ''
+	let categories = ''
+	let filterItems = document.querySelectorAll('.journal__block1LeftSections .sections__blocksBlock')
+	if(filterItems.length > 0) {
+		for(let item of filterItems) {
+			item.addEventListener('mouseup', () => filter(item.id, item.innerText) )
+		}
+		categories = document.querySelector('#blockCat')
+		mainBlock = document.querySelectorAll(`.filterBlock`)
+	}
+
+
+	function resetFilter() {
+		let blocksUpper = document.querySelectorAll('.sections__blocksBlock.filtered')
+		let cats = document.querySelectorAll('#blockCat > span')
+		let news = document.querySelectorAll(`.filterBlock .horizontalBar > .filtered`)
+		let filterBlock = document.querySelectorAll('.filterBlock')
+		let catBlock = document.querySelector('.journal__block1LeftNewsCategories')
+
+
+		catBlock.classList.remove('disp')
+		for(let item of cats) {
+			item.remove()
+		}
+		for(let item of filterBlock) {
+			item.classList.remove('filtered')
+		}
+		for(let item of blocksUpper) {
+			item.classList.remove('filtered')
+		}
+		for(let item of news) {
+			item.classList.remove('filtered')
+		}
+	}
+
+	let reset = document.querySelector('#resetFilter')
+	if(reset !== null) {
+		reset.addEventListener('mouseup', resetFilter)
+	}
+
+	function popularUnfold() {
+		let popularNews = document.querySelector('.popular')
+
+		popularNews.classList.toggle('open')
+	}
+
+	let popular__hidden = document.querySelector('.popular__hidden')
+	if (popular__hidden !== null) {
+		popular__hidden.addEventListener('mouseup', popularUnfold)
+	}
 })
